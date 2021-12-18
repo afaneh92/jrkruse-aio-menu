f496aea2cf94b9c83ad7290ef6135b5d1aae6a1f
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2021-12-18 10:10:48 +0200

    Switch to canary

--- bin/safetynet/post-fs-data.sh
+++ bin/safetynet/post-fs-data.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
-# Enable magiskhide
-magisk magiskhide enable
+# Remove Play Services from DenyList, otherwise the Zygisk module won't load
+magisk --denylist rm com.google.android.gms
